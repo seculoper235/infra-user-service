f58940053c0f9825f4063332c6a0596333819387
seculoper <mir8725@naver.com>
2025-04-13 11:30:46 +0900

    CORS 적용

---
+++ src/test/java/com/example/infrauserservice/environment/WebIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.example.infrauserservice.environment;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Disabled
+public class WebIntegrationTest {
+}
